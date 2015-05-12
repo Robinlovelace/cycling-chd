7c42398b1545f667a122a6d8b0cdde7859036fa4
Robinlovelace <rob00x@gmail.com>
2015-05-12 10:31:48 +0100

    Added packages to setup

--- setup.R
+++ setup.R
@@ -1,4 +1,4 @@
 # Aim load-in the data
-pkgs <- c("readxl", "stplanr", "raster", "rgdal", "dplyr")
+pkgs <- c("readxl", "stplanr", "raster", "rgdal", "dplyr", "downloader", "tmap")
 # install.packages(pkgs) uncomment to install these packages
 lapply(pkgs, library, character.only = TRUE)
