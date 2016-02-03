163332e9d6d41fa4bca6c9f71a0f8e3a415b90f9
Robin <rob00x@gmail.com>
2016-02-03 16:13:58 +0000

    Add script to load lab names

---
+++ R/export-minap-labs.R
@@ -0,0 +1,24 @@
+# Aim: load and 'scramble' some minap data
+
+library(readstata13)
+f <- "N:\\Faculty-of-Medicine-and-Health/LIGHT/Cardiovascular Epidemiology/Robin Lovelace/Sample_RL.dta"
+df <- read.dta13(f)
+names(df)
+labname <- get.label.name(df)
+labname <- labname[labname != ""]
+
+library(jsonlite)
+labout <- as.list(1:length(labname))
+for(i in 1:length(labname)){
+  labout[[i]] <- readstata13::get.label(df, label.name = labname[i])
+  labout[[i]] <- paste0(labout[[i]], " = ", names(readstata13::get.label(df, label.name = labname[i])))
+}
+
+names(labout)
+labout <- setNames(labout, labname)
+
+x <- toJSON(labout, pretty = T)
+writeLines(x, "labnames.json")
+
+set.seed(54)
+write.csv(df[sample(x = nrow(df), size = 100),], "testdat.csv")
