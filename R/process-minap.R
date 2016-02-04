b67a527b2d1d2227dafe72696c66283fe6dcb422
markagreen <mark.green@liverpool.ac.uk>
2016-02-04 15:08:03 +0000

    MG: Started R file to process data

---
+++ R/process-minap.R
@@ -0,0 +1,11 @@
+#################################################
+###### Myocardial Infarction Cycling Paper ######
+#################################################
+
+# Set working directory
+setwd("M:/GitHub/cycling-chd") # Mark
+
+# Load data
+df <- read.csv("./data/testdat.csv")
+
+
