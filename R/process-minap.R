4031ea27d2bcdb0181e6f7060a0dd99bcf3bf0cf
Robin <rob00x@gmail.com>
2016-06-29 09:06:35 +0100

    Add msoa data and geo join

--- R/process-minap.R
+++ R/process-minap.R
@@ -5,26 +5,51 @@
 # Libraries
 library(plyr)
 library(data.table)
+library(sp)
 
 # Load data
-sample_data <- readRDS("data/minap-data.Rds")
+sample_data <- readRDS("data/minap-sample.Rds")
 
 # Subset data to variables required
+names(sample_data)
 vars <- c("year", "age", "sex", "easting", "northing") # Will need to add more later
-subset_data = sample_data[vars]
+minap = sample_data[vars]
 
 # Keep only years interested in [need to make a decision on this]
-subset_data <- subset_data[subset_data$year>2009,]
+minap <- minap[minap$year>2009,]
+# Remove observations with no location
+minap = minap[!is.na(minap$easting) & !is.na(minap$easting),]
 
 ## RL - need to attach M/LSOA codes here ##
-
+library(sp)
+summary(minap$easting)
+coords = cbind(minap$easting * 100, minap$northing * 100)
+minap_sp = SpatialPointsDataFrame(coords = coords, data = minap)
+bbox(minap_sp)
+proj4string(minap_sp) = CRS("+init=epsg:27700")
+minap_sp = spTransform(x = minap_sp, CRSobj = CRS("+init=epsg:4326"))
+bbox(minap_sp)
+las = readRDS("data/las-geo-mode.Rds")
+names(las)
+# msoas = readRDS("../pct-bigdata/ukmsoas-scenarios.Rds")
+# names(msoas)
+# msoas@data = msoas@data[1:13]
+# saveRDS(msoas, "data/msoas.Rds")
+msoas = readRDS("data/msoas.Rds")
+names(msoas)
+plot(las, lwd = 3)
+plot(msoas, add = T) # just english msoas for now
+plot(minap_sp, col = "red", add = T)
+o = over(minap_sp, msoas)
+names(o)
+minap = cbind(minap, o[c("geo_code", "All", "Car", "Bicycle", "foot")])
 
 # Create age bands
-subset_data$age_band <- cut(subset_data[, "age"], c(-1, 15.5, 24.5, 34.5, 44.5, 54.5, 64.5, 74.5, 121),
+minap$age_band <- cut(minap[, "age"], c(-1, 15.5, 24.5, 34.5, 44.5, 54.5, 64.5, 74.5, 121),
                             labels=c("0-15","16-24","25-34","35-44","45-54","55-64","65-74","75+"))
 
 # Aggregate counts to M/LSOAs
-dt <- data.table(subset_data)
+dt <- data.table(minap)
 data <- dt[, list(admissions=.N), by = c("sex", "age_band", "year", "msoa_code")]
 data <- as.data.frame(data)
 
