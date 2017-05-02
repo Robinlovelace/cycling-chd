bd4d08f2d71313997a2e362c769b5dfd03fea86d
markagreen <mark.green@liverpool.ac.uk>
2017-05-02 16:38:40 +0100

    MG: Few edits on analyses files

---
+++ R/results_tables_mag.R
@@ -0,0 +1,12 @@
+### Results ###
+
+# Libraries
+
+
+# Load data
+res_f_01 <- readRDS("./la_results/yearly_results_f.Rds") # Females 2001 exposure
+res_f_01
+
+res_m_01 <- readRDS("./la_results/yearly_results_m.Rds") # Females 2001 exposure
+res_m_01
+
